7863f43eb3d0a4807ef7f9fe2cd483919ce03766
Sylidar <suntasak@live.com>
2015-09-30 13:23:41 -0500

    drdfdfgdfg
    dfgdfgg

---
+++ Assets/AssetButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssetButton : MonoBehaviour
+{
+    public GameObject assetbackground;
+    private bool toggle = false;
+        public void AssetMenuToggleClicked()
+        {
+        if (toggle == false)
+        {
+            CancelInvoke("SlideDown");
+            InvokeRepeating("SlideUp", 0.0f, .0001f);
+            toggle = true;
+        }
+        else
+        {
+            CancelInvoke("SlideUp");
+            InvokeRepeating("SlideDown", 0.0f, .0001f);
+            toggle = false;
+        }
+    }
+        void Update()
+        {
+            if ((assetbackground.transform.position.y > -.88) && (toggle == true))
+            {
+                CancelInvoke("SlideUp");
+            assetbackground.transform.position = new Vector3(0.00f,-.68f,-2.00f);
+            }
+            if ((assetbackground.transform.position.y < -8.05) && (toggle == false))
+                {
+                CancelInvoke("SlideDown");
+                assetbackground.transform.position = new Vector3(0.00f, -8.07f, -2.00f);
+            }
+    }
+        void SlideUp()
+        { 
+            assetbackground.transform.Translate(Vector3.up * 6 * Time.deltaTime);
+        }
+        void SlideDown()
+        {
+            assetbackground.transform.Translate(Vector3.up * -5 * Time.deltaTime);
+        }
+}
---
+++ Assets/Tapcity.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Tapcity : MonoBehaviour
+{
+    public int money,chinaPoints,polFavors,smMultiplier;
+    public SpriteRenderer trump1,trump2, trump3, trump4, trump5, trump6;
+    public  Text dollarcount;
+    private int rand;   
+    private int oldrand;
+    void Start ()
+    {
+        money = 1;
+        chinaPoints = 0;
+        polFavors = 0;
+        smMultiplier = 1;
+        rand = 1;
+	}
+
+    void OnMouseDown()
+    {
+        money += 1 * smMultiplier;
+        dollarcount.text = "$" + money;
+        oldrand = rand;
+        while(oldrand == rand)
+        {
+           rand = Random.Range(1, 7);
+        }
+        trump1.gameObject.SetActive(false);
+        trump2.gameObject.SetActive(false);
+        trump3.gameObject.SetActive(false);
+        trump4.gameObject.SetActive(false);
+        trump5.gameObject.SetActive(false);
+        trump6.gameObject.SetActive(false);
+        if(rand == 1)
+            trump1.gameObject.SetActive(true);
+        if (rand == 2)
+            trump2.gameObject.SetActive(true);
+        if (rand == 3)
+            trump3.gameObject.SetActive(true);
+        if (rand == 4)
+            trump4.gameObject.SetActive(true);
+        if (rand == 5)
+            trump5.gameObject.SetActive(true);
+        if (rand == 6)
+            trump6.gameObject.SetActive(true);
+    }   
+}
